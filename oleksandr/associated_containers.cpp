9f1fdd61f454e670a3bd406888caa141fe8c9935
Oleksandr Senkovych <bjsenya@gmail.com>
2019-11-08 15:30:55 +0100

    Fix slowness

--- oleksandr/associated_containers.cpp
+++ oleksandr/associated_containers.cpp
@@ -131,14 +131,14 @@ BENCHMARK_TEMPLATE(InDictionary, Set<std::set<std::string_view>>);
 BENCHMARK_TEMPLATE(InDictionary, Set<std::unordered_set<std::string_view>>);
 BENCHMARK_TEMPLATE(InDictionary, Set<std::set<std::string>>);
 BENCHMARK_TEMPLATE(InDictionary, Set<std::unordered_set<std::string>>);
-BENCHMARK_TEMPLATE(InDictionary, OpenAddressingMap<std::string>);
+BENCHMARK_TEMPLATE(InDictionary, OpenAddressingMap);
 BENCHMARK_TEMPLATE(NotInDictionary, Vector<LinearSearch>);
 BENCHMARK_TEMPLATE(NotInDictionary, Vector<BinarySearch>);
 BENCHMARK_TEMPLATE(NotInDictionary, Set<std::set<std::string_view>>);
 BENCHMARK_TEMPLATE(NotInDictionary, Set<std::unordered_set<std::string_view>>);
 BENCHMARK_TEMPLATE(NotInDictionary, Set<std::set<std::string>>);
 BENCHMARK_TEMPLATE(NotInDictionary, Set<std::unordered_set<std::string>>);
-BENCHMARK_TEMPLATE(NotInDictionary, OpenAddressingMap<std::string>);
+BENCHMARK_TEMPLATE(NotInDictionary, OpenAddressingMap);
 
 int main(int argc, char** argv) {
   static const int DICT_SIZE = 100'000;
--- oleksandr/open_addressing_map.hpp
+++ oleksandr/open_addressing_map.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 #include <algorithm>
+#include <iostream>
 #include <optional>
 #include <vector>
 
-template <typename Value>
 struct OpenAddressingMap
 {
   template <typename Iterator>
   OpenAddressingMap(Iterator begin, Iterator end) {
-    data.resize(2*std::distance(begin, end));
+    data.resize(2 * std::distance(begin, end));
     std::for_each(begin, end, [this](auto value) { insert(value); });
   }
 
@@ -17,27 +17,28 @@ struct OpenAddressingMap
   OpenAddressingMap(const Container& c)
     : OpenAddressingMap(std::begin(c), std::end(c)) {}
 
-  void insert(const Value& value) {
-    auto hash = std::hash<Value>()(value);
-    auto position = hash % data.size();
-
-    for (auto it = data.begin() + position; it != data.end(); ++it) {
+  void insert(const std::string& value) {
+    auto start = getPosition(data, value);
+    for (auto it = start; it != start - 1;) {
       auto& optionalValue = *it;
       if (!optionalValue.has_value()) {
         optionalValue = value;
+        ++dataSize;
         return;
+      }
+
+      ++it;
+      if (it == data.end()) {
+        it = data.begin();
       }
     }
 
     throw std::runtime_error("No space");
   }
 
-  template <typename T>
-  bool has(const T& value) const {
-    auto hash = std::hash<T>()(value);
-    auto position = hash % data.size();
-
-    for (auto it = data.begin() + position; it != data.end(); ++it) {
+  bool has(std::string_view value) const {
+    auto start = getPosition(data, value);
+    for (auto it = start; it != start - 1;) {
       auto& optionalValue = *it;
       if (!optionalValue.has_value()) {
         return false;
@@ -45,6 +46,11 @@ struct OpenAddressingMap
 
       if (optionalValue.value() == value) {
         return true;
+      }
+
+      ++it;
+      if (it == data.end()) {
+        it = data.begin();
       }
     }
 
@@ -56,5 +62,14 @@ struct OpenAddressingMap
   }
 
 private:
-  std::vector<std::optional<Value>> data;
+  template <typename T>
+  static auto getPosition(T&& t, std::string_view value) -> decltype(t.begin()) {
+    auto hash = std::hash<std::string_view>()(value);
+    auto position = hash % t.size();
+
+    return t.begin() + position;
+  }
+
+  std::vector<std::optional<std::string>> data;
+  size_t dataSize = 0;
 };
